5ceb9e245991a84a59cad4f744a07e65c6555f85
Shelts <Shelts@rpi.edu>
2015-09-12 15:16:59 -0400

    update

--- compare_hists.py
+++ compare_hists.py
@@ -5,7 +5,7 @@ import os
 args = [1, 0.5, 0.2, 30, 0.2]
 
 #sim_time      = [0.0001, 0.0001, 4, 4]
-sim_time      = [0.0001, 4]
+sim_time      = [4, 4]
 N             = 2
 back_time     = str(args[0])
 r0            = str(args[1])
@@ -22,21 +22,21 @@ os.system("make -j ")
 os.chdir("../")
 
 #file:///home/sidd/Desktop/research/lua/Null_even.lua
-#file:///home/sidd/Desktop/research/lua/EMD_20k_isotropic_1_50.lua
+#file:///home/sidd/Desktop/research/lua/EMD_20k_isotropic_1_52.lua
 #same seed:
 for i in range(0, N):
     os.system("~/Desktop/research/nbody_test/bin/milkyway_nbody \
-        -f ~/Desktop/research/lua/Null_even.lua \
+        -f ~/Desktop/research/lua/EMD_20k_isotropic_1_52.lua \
         -h ~/Desktop/research/histogram_20kEMD_v150_" + str(i-1) +".hist \
         -z ~/Desktop/research/histogram_20kEMD_v150_" + str(i) +".hist \
-        -n 4 -P -x -e  9874565 -i "+ str(sim_time[i]) + " " + back_time + " " + r0 + " " + light_r_ratio + " " + mass + " " + mass_ratio + " \
+        -n 4 -P -x -e  7584962 -i "+ str(sim_time[i]) + " " + back_time + " " + r0 + " " + light_r_ratio + " " + mass + " " + mass_ratio + " \
         2>>~/Desktop/research/piped_output.txt")
 
 
 ###diff seed
 #for i in range(0, N):
     #os.system("~/Desktop/research/nbody_test/bin/milkyway_nbody \
-        #-f ~/Desktop/research/lua/EMD_20k_isotropic_1_50.lua \
+        #-f ~/Desktop/research/lua/EMD_20k_isotropic_1_52.lua \
         #-h ~/Desktop/research/histogram_20kEMD_v150_" + str(i-1) +".hist \
         #-z ~/Desktop/research/histogram_20kEMD_v150_" + str(i) +".hist \
         #-n 4 -P -x  -i "+ str(sim_time[i]) + " " + back_time + " " + r0 + " " + light_r_ratio + " " + mass + " " + mass_ratio + " \
--- make_and_run.sh
+++ make_and_run.sh
@@ -2,18 +2,21 @@
 #     rm -r nbody_test
 #     mkdir nbody_test
 #     rm ~/Desktop/research/nbody_test/bin/milkyway_nbody
-    cd ~/Desktop/research/nbody_test
-    cmake -DCMAKE_BUILD_TYPE=Release  -DNBODY_GL=ON -DBOINC_APPLICATION=OFF -DSEPARATION=OFF -DNBODY_OPENMP=ON    ~/Desktop/research/milkywayathome_client/
-    make -j 
+#     cd ~/Desktop/research/nbody_test
+#     cmake -DCMAKE_BUILD_TYPE=Release  -DNBODY_GL=ON -DBOINC_APPLICATION=OFF -DSEPARATION=OFF -DNBODY_OPENMP=ON    ~/Desktop/research/milkywayathome_client/
+#     make -j 
 
     cd ~/Desktop/research/nbody_test/bin
 
     time  ./milkyway_nbody \
     -f ~/Desktop/research/lua/Null_even.lua \
     -o output_test.out \
-    -n 4 -x  -i   0.01 1 0.5 0.2 30 .2 \
-    2>>~/Desktop/research/piped_output.txt  
-    rm ./milkyway_nbody
+    -n 6 -x -u \
+    --visualizer-args --print-frames=test \
+    -e 123456 -i 4 1 0.5 0.2 30 .2 \
+#     2>>~/Desktop/research/piped_output.txt  
+#     rm ./milkyway_nbody
+
 
 #     mv rho.txt  ~/Desktop/research/quick_plots
 #     mv dist.txt  ~/Desktop/research/quick_plots
@@ -34,6 +37,7 @@
 python outputparser.py ~/Desktop/research/nbody_test/bin/output_test.out
 rm dark_matter.dat
 rm light_matter.dat
-# 	/Desktop/research/lua/EMD_20k_isotropic_1_50.lua 125896   -e 125896
+# 	/Desktop/research/lua/EMD_20k_isotropic_1_52.lua 125896   -e 125896
 # 	/Desktop/research/lua/Null_even.lua    
 # -z ~/Desktop/research/histogram_20kEMD_v150.hist \
+# -z ~/Desktop/research/histogram_20kEMD_v152_ft4_bt1_rp5_rrp2_m30_mrp2_seed123456 \
