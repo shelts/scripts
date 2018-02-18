c85168164d61992d4f6d775b191a1c10d213ff09
shelts <shelts@rpi.edu>
2018-02-18 15:09:14 -0500

    update

--- create_data_hist/beta_bins.py
+++ create_data_hist/beta_bins.py
@@ -19,16 +19,11 @@ class bin_betas:#class to make histogram of betas in each bin
         
         # bin the on and off field seperately so you can adjust it until they have same number of bins #
         # these parameters should be kept the same. But the option is there.
-        self.beta_Nbins_OFF = 30
-        self.lower_OFF  = -5.0
-        self.upper_OFF = 5.0
+        self.beta_Nbins = 30
+        self.lower = -5.0
+        self.upper = 5.0
         
-        self.beta_Nbins_ON = 30
-        self.lower_ON = -5.0
-        self.upper_ON = 5.0
-        
-        self.bin_width_ON    = abs(self.lower_ON - self.upper_ON) / self.beta_Nbins_ON
-        self.bin_width_OFF   = abs(self.lower_OFF - self.upper_OFF) / self.beta_Nbins_OFF
+        self.bin_width    = abs(self.lower - self.upper) / self.beta_Nbins
         
         self.initialize_beta_bins(beta_coors_ON, beta_coors_OFF) # sets up the beta bins
         del beta_coors_ON, beta_coors_OFF # free up some space
@@ -43,19 +38,13 @@ class bin_betas:#class to make histogram of betas in each bin
         self.optimize()
         #self.plot_each_bin()
     def initialize_beta_bins(self, beta_coors_ON, beta_coors_OFF):
-        self.bin_centers_ON  = []
-        self.bin_centers_OFF = []
-        # initialize the beta bin centers ON field #
-        center  = self.lower_ON + self.bin_width_ON / 2.0
-        for i in range(0, self.beta_Nbins_ON): # initial beta bin centers On field
-            self.bin_centers_ON.append(center)
-            center += self.bin_width_ON
+        self.bin_centers = []
         
-        # initialize the beta bin centers OFF field #
-        center = self.lower_OFF + self.bin_width_OFF / 2.0
-        for i in range(0, self.beta_Nbins_OFF): # initial beta bin centers off field
-            self.bin_centers_OFF.append(center)
-            center += self.bin_width_OFF
+        # initialize the beta bin centers ON field #
+        center = self.lower +  self.bin_width / 2.0
+        for i in range(0, self.beta_Nbins): # initial beta bin centers On field
+            self.bin_centers.append(center)
+            center += self.bin_width
         
         # create array for storing beta count data #
         for i in range(0, self.lmda_bnd.Nbins):  
@@ -63,13 +52,10 @@ class bin_betas:#class to make histogram of betas in each bin
             self.binned_beta_OFF.append([])
             
             # initialize the counts to zero in each bin ON field #
-            for j in range(0, self.beta_Nbins_ON): # for each beta bins
+            for j in range(0, self.beta_Nbins): # for each beta bins
                 self.binned_beta_ON[i].append(0.0) # initialize the counts for the beta bins
-            
-            # initialize the counts to zero in each bin OFF field #
-            for j in range(0, self.beta_Nbins_OFF):
                 self.binned_beta_OFF[i].append(0.0)
-                
+            
             # send the beta coors for this lambda bin for binning #    
             self.binner(i, beta_coors_ON[i], beta_coors_OFF[i]) 
 
@@ -78,7 +64,7 @@ class bin_betas:#class to make histogram of betas in each bin
         for i in range(0, self.lmda_bnd.Nbins):
             self.binned_combined.append([])
             #print len(self.binned_beta_ON[i]), len(self.binned_beta_OFF[i])
-            for j in range(0, len(self.bin_centers_ON)):
+            for j in range(0, len(self.bin_centers)):
                 self.binned_combined[i].append(0)
                 self.binned_combined[i][j] = (self.binned_beta_ON[i][j] + self.binned_beta_OFF[i][j])
                 #print i, self.binned_beta_ON[i][j], self.binned_beta_OFF[i][j], self.binned_combined[i][j]
@@ -87,26 +73,26 @@ class bin_betas:#class to make histogram of betas in each bin
     
     def binner(self, lmbda_bin, coors_ON, coors_OFF): # (current lambda bin, beta coors on field, beta coors off field)
         for j in range(0, len(coors_ON)): # for each beta coordinate in the lmda bin
-            for k in range(0, self.beta_Nbins_ON): # for each beta bin
-                lower_bound = (self.bin_centers_ON[k] - self.bin_width_ON / 2.0) # bin bounds
-                upper_bound = (self.bin_centers_ON[k] + self.bin_width_ON / 2.0)
+            for k in range(0, self.beta_Nbins): # for each beta bin
+                lower_bound = (self.bin_centers[k] - self.bin_width / 2.0) # bin bounds
+                upper_bound = (self.bin_centers[k] + self.bin_width / 2.0)
                 
                 if(coors_ON[j] >= lower_bound  and coors_ON[j] <= upper_bound): # check if beta coor is in the bin
                     self.binned_beta_ON[lmbda_bin][k] += 1.0
         
         for j in range(0, len(coors_OFF)): # for each beta coordinate in the lmda bin
-            for k in range(0, self.beta_Nbins_OFF): # for each beta bin
-                lower_bound = (self.bin_centers_OFF[k] - self.bin_width_OFF / 2.0)
-                upper_bound = (self.bin_centers_OFF[k] + self.bin_width_OFF / 2.0)
-                #print lower_bound, upper_bound
+            for k in range(0, self.beta_Nbins): # for each beta bin
+                lower_bound = (self.bin_centers[k] - self.bin_width / 2.0)
+                upper_bound = (self.bin_centers[k] + self.bin_width / 2.0)
                 if(coors_OFF[j] >= lower_bound  and coors_OFF[j] <= upper_bound):
                     self.binned_beta_OFF[lmbda_bin][k] += 1.0
+                #print lower_bound, upper_bound
     
     def optimize(self):
         iters = 40000
         os.system("rm -r stream_beta_plots/lamb*")
         for i in range(0, self.lmda_bnd.Nbins):
-            self.fit = diff_evo(self.bin_centers_ON , self.binned_combined[i], iters )
+            self.fit = diff_evo(self.bin_centers , self.binned_combined[i], iters )
             self.fit_paras = self.fit.pop.cur_pop[self.fit.best_index]
             print self.fit_paras
             self.plot_each_bin(i) # plot each lambda bin seperately
@@ -117,7 +103,7 @@ class bin_betas:#class to make histogram of betas in each bin
         w = 0.25
         #test_dat = test_data()
         plt.figure()
-        plt.xlim(self.lower_OFF, self.upper_OFF)
+        plt.xlim(self.lower, self.upper)
         plt.ylim(0, 400)
         plt.ylabel("counts")
         plt.xlabel(r"$\beta_{Orphan}$")
@@ -127,9 +113,9 @@ class bin_betas:#class to make histogram of betas in each bin
             fit_paras = self.fit_paras
             fit_xs, fit_fs = self.fit.generate_plot_points()
             plt.plot(fit_xs,  fit_fs, color='k',linewidth = 2, alpha = 1., label = 'paras = ' + str(round(fit_paras[0], 2)) + ' ' + str(round(fit_paras[1], 2)) + ' ' + str(round(fit_paras[2], 2)) + ' ' + str(round(fit_paras[3], 2)) + ' ' + str(round(fit_paras[4], 2)) )
-            plt.bar(self.bin_centers_OFF, self.binned_combined[i], width=w, color='m', alpha = 1., label = 'C')
-            plt.bar(self.bin_centers_OFF, self.binned_beta_OFF[i], width=w, color='r', alpha = 0.5, label = 'OFF')
-            plt.bar(self.bin_centers_ON,  self.binned_beta_ON[i], width=w, color='b', alpha = 0.5, label = 'ON')
+            plt.bar(self.bin_centers, self.binned_combined[i], width=w, color='m', alpha = 1., label = 'C')
+            plt.bar(self.bin_centers, self.binned_beta_OFF[i], width=w, color='r', alpha = 0.5, label = 'OFF')
+            plt.bar(self.bin_centers, self.binned_beta_ON[i], width=w, color='b', alpha = 0.5, label = 'ON')
             plt.legend()
             plt.savefig('stream_beta_plots/lambda_bin_' + str(i) + '_' + str(self.lmda_bnd.bin_centers[i]) + '.png', format = 'png')
             plt.close()
@@ -141,9 +127,9 @@ class bin_betas:#class to make histogram of betas in each bin
                 plt.ylim(0, 400)
                 plt.ylabel("counts")
                 plt.xlabel(r"$\beta_{Orphan}$")
-                plt.bar(self.bin_centers_OFF, self.binned_combined[i], width=w, color='m', alpha = 1., label = 'C')
-                plt.bar(self.bin_centers_OFF, self.binned_beta_OFF[i], width=w, color='r', alpha = 0.5, label = 'OFF')
-                plt.bar(self.bin_centers_ON,  self.binned_beta_ON[i], width=w, color='b', alpha = 0.5, label = 'ON')
+                plt.bar(self.bin_centers, self.binned_combined[i], width=w, color='m', alpha = 1., label = 'C')
+                plt.bar(self.bin_centers, self.binned_beta_OFF[i], width=w, color='r', alpha = 0.5, label = 'OFF')
+                plt.bar(self.bin_centers, self.binned_beta_ON[i], width=w, color='b', alpha = 0.5, label = 'ON')
                 #plt.scatter(test_dat.xs, test_dat.fs, s = 0.9, color = 'k')
                 plt.legend()
                 plt.savefig('stream_beta_plots/lambda_bin_' + str(i) + '_' + str(self.lmda_bnd.bin_centers[i]) + '.png', format = 'png')
@@ -161,8 +147,8 @@ class bin_betas:#class to make histogram of betas in each bin
         ax.set_zlabel("counts")
         ax.set_ylabel(r"$\Lambda_{Orphan}$")
         for i in range(0, self.lmda_bnd.Nbins):
-            ax.bar(self.bin_centers_OFF, self.binned_beta_OFF[i], width=w,zs=self.lmda_bnd.bin_centers[i], zdir='y', color='r', alpha = 0.75)
-            ax.bar(self.bin_centers_ON, self.binned_beta_ON[i], width=w,  zs=self.lmda_bnd.bin_centers[i], zdir='y',  color='b', alpha = 0.5, )
+            ax.bar(self.bin_centers, self.binned_beta_OFF[i], width=w,zs=self.lmda_bnd.bin_centers[i], zdir='y', color='r', alpha = 0.75)
+            ax.bar(self.bin_centers, self.binned_beta_ON[i], width=w,  zs=self.lmda_bnd.bin_centers[i], zdir='y',  color='b', alpha = 0.5, )
         plt.savefig('stream_beta_plots/lambda_beta_bins.png', format = 'png')
         plt.show()
         
@@ -174,63 +160,56 @@ class bin_betas:#class to make histogram of betas in each bin
         for i in range(0, self.lmda_bnd.Nbins): # for each lambda bin
             off_field_ave = 0.0
             bns = 0.0
-            for j in range(0, self.beta_Nbins_OFF): # for each of the beta bins
+            for j in range(0, self.beta_Nbins): # for each of the beta bins
                 if(self.binned_beta_OFF[i][j] > 0.0): # make sure ave includes only bins with counts
                     off_field_ave += self.binned_beta_OFF[i][j]
                     bns += 1.0
             bins_with_counts.append(bns)
             self.bin_off_field_aves.append(off_field_ave)
-        
-        for i in range(0, self.lmda_bnd.Nbins):
             if(float(bins_with_counts[i]) > 0.0):
                 self.bin_off_field_aves[i] = self.bin_off_field_aves[i] / float(bins_with_counts[i])
-        #print self.bin_off_field_aves
+        
         return 0
     
     def correction(self):
         for i in range(0, self.lmda_bnd.Nbins):
-            for j in range(0, self.beta_Nbins_ON):
-                if(self.binned_beta_ON[i][j] > 0.0):
-                    self.binned_beta_ON[i][j] -= self.bin_off_field_aves[i] * 1.
-                    if(self.binned_beta_ON[i][j] < 0.0):
-                        self.binned_beta_ON[i][j] = 0
+            for j in range(0, self.beta_Nbins):
+                self.binned_beta_ON[i][j]  -= self.bin_off_field_aves[i] * 1.
+                self.binned_beta_OFF[i][j] -= self.bin_off_field_aves[i] * 1.
+                if(self.binned_beta_ON[i][j] < 0.0):
+                    self.binned_beta_ON[i][j] = 0
+                if(self.binned_beta_OFF[i][j] < 0.0):
+                    self.binned_beta_OFF[i][j] = 0
 
-        for i in range(0, self.lmda_bnd.Nbins):
-            for j in range(0, self.beta_Nbins_OFF):
-                if(self.binned_beta_OFF[i][j] > 0.0):
-                    self.binned_beta_OFF[i][j] -= self.bin_off_field_aves[i] * 1.
-                    if(self.binned_beta_OFF[i][j] < 0.0):
-                        self.binned_beta_OFF[i][j] = 0
         return 0
     
     def den_correction(self):
         for i in range(0, self.lmda_bnd.Nbins):
-            for j in range(0, self.beta_Nbins_ON):
-                if(self.binned_beta_ON[i][j] > 0.0):
-                    self.binned_beta_ON[i][j] -= self.star_density[i] * 1.
-                    if(self.binned_beta_ON[i][j] < 0.0):
-                        self.binned_beta_ON[i][j] = 0
+            width = abs(self.lmda_bnd.bin_uppers[i] - self.lmda_bnd.bin_lowers[i]) #width of each lambda bin
+            for j in range(0, self.beta_Nbins):
+                area = self.bin_width * width
+                interlopers = self.star_density[i] * area
+                self.binned_beta_ON[i][j]  -= interlopers
+                self.binned_beta_OFF[i][j] -= interlopers
+                if(self.binned_beta_ON[i][j] < 0.0):
+                    self.binned_beta_ON[i][j] = 0
+                if(self.binned_beta_OFF[i][j] < 0.0):
+                    self.binned_beta_OFF[i][j] = 0
 
-        for i in range(0, self.lmda_bnd.Nbins):
-            #print self.binned_beta_OFF[i]
-            for j in range(0, self.beta_Nbins_OFF):
-                if(self.binned_beta_OFF[i][j] > 0.0):
-                    self.binned_beta_OFF[i][j] -= self.star_density[i] * 1.
-                    if(self.binned_beta_OFF[i][j] < 0.0):
-                        self.binned_beta_OFF[i][j] = 0
-            #print self.binned_beta_OFF[i], '\n'
         return 0
     
     def off_field_star_density(self):
         self.star_density = []
-        length = abs(self.lower_OFF - self.upper_OFF) # length of each beta bin
+        length = abs(self.lower - self.upper) # length of each beta bin
         for i in range(0, self.lmda_bnd.Nbins):
             width = abs(self.lmda_bnd.bin_uppers[i] - self.lmda_bnd.bin_lowers[i]) #width of each lambda bin
-            area = width * length
-            self.star_density.append(0)
-            for j in range(0, self.beta_Nbins_OFF):
+            self.star_density.append(0.0)
+            area = 0.0
+            for j in range(0, self.beta_Nbins):
                 if(self.binned_beta_OFF[i][j] > 0):
-                    self.star_density[i] += self.binned_beta_OFF[i][j]
-            self.star_density[i] = self.star_density[i] / area
-        #print star_density
+                    self.star_density[i] += float(self.binned_beta_OFF[i][j]) # add the counts of the bin to the total count
+                    area += self.bin_width # sum of the lengths of off field with stars
+                    print area, width
+            area = width * area # get the area
+            self.star_density[i] = float(self.star_density[i]) / area
         
