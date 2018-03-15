6a9cd412fe1b7d4ce437fdafd2d617060a877e16
shelts <shelts@rpi.edu>
2018-03-15 19:34:31 -0400

    update

--- create_data_hist/removed.py
+++ create_data_hist/removed.py
@@ -45,3 +45,54 @@
         plt.show()
         
         return 0
+
+
+
+    def beta_dispersion(self):
+        for i in range(0, len(self.beta_diff.sums)):
+            sums = self.beta_diff.sums[i]
+            sqsums = self.beta_diff.sqsums[i]
+            
+            N = self.beta_diff.binN[i]
+            if(N > 2):
+                dispsq = sqsums / (N - 1.)
+                #print dispsq, sqsums, sums**2.
+                dispsq -= ( N / (N - 1.) ) * (sums / N)**2.
+                #print dispsq, ( N / (N - 1.) ) * (sums / N)**2., N, '\n'
+                dispsq = dispsq**0.5
+                self.beta_diff.disp.append(dispsq)
+            else:
+                self.beta_diff.disp.append(-1)
+                
+                
+                
+    def den_correction(self):
+        for i in range(0, self.lmda_bnd.Nbins):
+            width = abs(self.lmda_bnd.bin_uppers[i] - self.lmda_bnd.bin_lowers[i]) #width of each lambda bin
+            for j in range(0, self.beta_Nbins):
+                area = self.bin_width * width
+                interlopers = self.star_density[i] * area
+                self.binned_beta_combined[i][j] -= interlopers
+                    
+                #if(self.binned_beta_combined[i][j] < 0.0):
+                    #self.binned_beta_combined[i][j] = 0.0
+                #print '\t\t\t\t', self.binned_beta_ON[i][j], self.binned_beta_OFF[i][j], self.binned_beta_combined[i][j], interlopers
+
+        return 0
+    
+    def off_field_star_density(self):
+        self.star_density = []
+        length = abs(self.lower - self.upper) # length of each beta bin
+        for i in range(0, self.lmda_bnd.Nbins):
+            width = abs(self.lmda_bnd.bin_uppers[i] - self.lmda_bnd.bin_lowers[i]) #width of each lambda bin
+            self.star_density.append(0.0)
+            area = 0.0
+            
+            for j in range(0, self.beta_Nbins):
+                if(self.binned_beta_OFF[i][j] > 0):
+                    self.star_density[i] += float(self.binned_beta_OFF[i][j]) # add the counts of the bin to the total count
+                    area += self.bin_width # sum of the lengths of off field with stars of the bins that had counts
+            area = area * width # get the area
+            if(self.star_density[i] > 0):#if there was counts then divide the area. if no counts then density is zero anyway
+                self.star_density[i] = float(self.star_density[i]) / area
+        
